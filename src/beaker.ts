ccdc566a690647e5d754aca51855424d47f68859
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-10-06 17:36:19 -0400

    stuff

--- src/beaker.ts
+++ src/beaker.ts
@@ -1,7 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/env npx tsx 
 
 import { Command } from 'commander';
-import { generateApplicationClient } from './index.js';
+import { generateApplicationClient } from '.';
 
 import * as fs from 'fs';
 import * as path from 'path';
